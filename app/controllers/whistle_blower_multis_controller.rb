2172ad20689697436b7b798fd26a655c602b09ab
cemmanuel1 <cemmanuel1@gmail.com>
2014-11-17 07:28:41 -0800

    Style and create redirect cleanup

--- app/controllers/whistle_blower_multis_controller.rb
+++ app/controllers/whistle_blower_multis_controller.rb
@@ -6,17 +6,10 @@ class WhistleBlowerMultisController < ApplicationController
   def create
     @whistle_blower_multi = WhistleBlowerMulti.new(params[:whistle_blower_multi])
 
-    if params[:whistle_blower_multi][:check_box] == "1"
       @whistle_blower_multi.request = request
-      if @whistle_blower_multi.deliver
-        flash.now[:error] = nil
-        # flash.now[:notice] = 'Thank you for your message!'
-      else
-        flash.now[:error] = 'Cannot send message.'
+      if !@whistle_blower_multi.deliver
         render :new
+        flash.now[:error] = 'Cannot send message.'
       end
-    else
-      flash.now[:error] = 'Must check box to continue'
-    end
   end
 end
--- app/controllers/whistle_blower_singles_controller.rb
+++ app/controllers/whistle_blower_singles_controller.rb
@@ -6,10 +6,7 @@ class WhistleBlowerSinglesController < ApplicationController
   def create
     @whistle_blower_single = WhistleBlowerSingle.new(params[:whistle_blower_single])
     @whistle_blower_single.request = request
-    if @whistle_blower_single.deliver
-      flash.now[:error] = nil
-      # flash.now[:notice] = 'Thank you for your message!'
-    else
+    if !@whistle_blower_single.deliver
       flash.now[:error] = 'Cannot send message.'
       render :new
     end
